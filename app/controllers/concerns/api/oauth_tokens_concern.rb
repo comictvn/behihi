36b44f62ae7cb7f86cbbba9f250c63ddcb657e4e
Berni (Jitera) <developers@jitera.com>
2024-05-04 12:11:39 +0000

    fix: 1 issues from deployment report

---
+++ app/controllers/concerns/api/oauth_tokens_concern.rb
@@ -0,0 +1,13 @@
+
+# typed: ignore
+module Api
+  module OauthTokensConcern
+    extend ActiveSupport::Concern
+
+    included do
+      # Define methods to be included in the controller where this concern is included.
+    end
+
+    # Define any other methods that can be used across controllers.
+  end
+end
