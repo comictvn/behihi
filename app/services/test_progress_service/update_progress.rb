91b4c53da185fb0c15641d4cf78ed86c51df006e
Pavia (Jitera) <developers@jitera.com>
2024-05-04 11:34:32 +0000

    feat[service]: Update test progress bar

--- app/services/test_progress_service/update_progress.rb
+++ app/services/test_progress_service/update_progress.rb
@@ -5,11 +5,19 @@ class TestProgressService
       user = User.find(user_id)
       test_progress = TestProgress.find_by!(user: user)
 
-      test_progress.update!(current_question_number: current_question_number)
-      total_questions = test_progress.total_questions
-      progress_percentage = (current_question_number.to_f / total_questions).round(2)
+      # Update the current_question_number only if it has changed
+      if test_progress.current_question_number != current_question_number
+        test_progress.update!(current_question_number: current_question_number)
+      end
 
-      test_progress.update!(progress_percentage: progress_percentage)
+      total_questions = test_progress.total_questions
+      # Calculate the progress percentage and multiply by 100 to convert to percentage
+      progress_percentage = ((current_question_number.to_f / total_questions) * 100).round(2)
+
+      # Update the progress_percentage only if it has changed
+      if test_progress.progress_percentage != progress_percentage
+        test_progress.update!(progress_percentage: progress_percentage)
+      end
 
       { current_question_number: test_progress.current_question_number, progress_percentage: progress_percentage }
     end
