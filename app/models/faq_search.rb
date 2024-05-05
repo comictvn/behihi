4c94af973be808c7be27e9381fb3c958a01360b1
Libbey (Jitera) <developers@jitera.com>
2024-05-05 08:59:57 +0000

    feat[service]: Search topics

--- app/models/faq_search.rb
+++ app/models/faq_search.rb
@@ -1,10 +1,22 @@
+
 class FaqSearch < ApplicationRecord
   belongs_to :user
 
   # validations
+  validates :search_query, presence: true
+  validate :search_query_must_be_non_empty_string
 
   # end for validations
 
   class << self
+    def log_search(search_query, user_id)
+      create(search_query: search_query, user_id: user_id) if search_query.present? && search_query.is_a?(String) && !search_query.strip.empty?
+    end
+  end
+
+  private
+
+  def search_query_must_be_non_empty_string
+    errors.add(:search_query, 'must be a non-empty string') unless search_query.is_a?(String) && !search_query.strip.empty?
   end
 end
