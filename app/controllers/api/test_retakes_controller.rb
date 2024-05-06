9675c8e378922d24116f9d49dbc37e3dea50bdc6
Rosa (Jitera) <developers@jitera.com>
2024-05-06 01:20:07 +0000

    feat[api]: implement Initiate Test Retake

--- app/controllers/api/test_retakes_controller.rb
+++ app/controllers/api/test_retakes_controller.rb
@@ -1,10 +1,11 @@
 
 class Api::TestRetakesController < Api::BaseController
   before_action :doorkeeper_authorize!
+  before_action :validate_user_id, only: [:create]
 
   # POST /api/test_retakes
   def create
-    user_id = params[:user_id].to_i
+    user_id = params[:userId].to_i
 
     begin
       message = TestProgressService::ResetTest.new(user_id).execute
@@ -30,4 +31,19 @@ class Api::TestRetakesController < Api::BaseController
       render json: { message: e.message }, status: :internal_server_error
     end
   end
+
+  private
+
+  def validate_user_id
+    unless params[:userId].to_s.match?(/\A\d+\z/)
+      render json: { message: "Invalid user ID format." }, status: :bad_request and return
+    end
+
+    unless User.exists?(params[:userId])
+      render json: { message: "User not found." }, status: :not_found and return
+    end
+  end
+
+  # Other private methods (if any)
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  namespace :api do
-    get '/test-review/:userId', to: 'test_reviews#show'
-  end
   get '/health' => 'pages#health_check'
   get 'api-docs/v1/swagger.yaml' => 'swagger#yaml'
+  post '/test-retake/:userId', to: 'test_retakes#create'
 end
