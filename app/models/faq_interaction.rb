04d1558fa3b5d2cb7e805c1a7d2e85cf8c9a5caa
Conni (Jitera) <developers@jitera.com>
2024-05-15 11:27:31 +0000

    feat[database]: update ERD from source code

--- app/models/faq_interaction.rb
+++ app/models/faq_interaction.rb
@@ -2,7 +2,7 @@ class FaqInteraction < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
-  enum interaction_type: %w[viewed searched know_clicked view search answer click comment like dislike feedback],
+  enum interaction_type: %w[viewed searched know_clicked view search answer click comment like dislike feedback WOW!! もしる skip],
        _suffix: true
 
   # validations
---
+++ app/models/new_table.rb
@@ -0,0 +1,8 @@
+class NewTable < ApplicationRecord
+  # validations
+
+  # end for validations
+
+  class << self
+  end
+end
